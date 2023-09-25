a75117ea0c68f1e371b718dfd358057db5246ad2
nestor-nurod <nestor.nurod@gmail.com>
2023-09-24 21:19:06 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Presentation from './components/Presentation.vue';
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <Presentation/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1920px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
---
+++ src/components/Presentation.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+    <section id="presentation" class="background-image">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="text-center">Karen Y Jair</h1>
+                </div>
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col-3 col-sm-3">
+                            <p>Dias</p>
+                        </div>
+                        <div class="col-3 col-sm-3">
+                            <p>Horas</p>
+                        </div>
+                        <div class="col-3 col-sm-3">
+                            <p>Minutos</p>
+                        </div>
+                        <div class="col-3 col-sm-3">
+                            <p>Segundos</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3 col-sm-3">
+                            <p>100</p>
+                        </div>
+                        <div class="col-3 col-sm-3">
+                            <p>12</p>
+                        </div>
+                        <div class="col-3 col-sm-3">
+                            <p>23</p>
+                        </div>
+                        <div class="col-3 col-sm-3">
+                            <p>12</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
